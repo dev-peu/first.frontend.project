31ebdfa569292a96987295e8da409ea4ae146aff
Dev-Peu <pedrocadast@gmail.com>
2023-01-24 20:11:46 -0300

    roteamento funcionando

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { HashRouter as Router } from "react-router-dom";
+import "./global.css";
+import Routes from "./routes/index.routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(
+  <Router>
+    <Routes />
+  </Router>
+);
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function Home() {
+  return <div className="container">aqui é a pagina home</div>;
+}
